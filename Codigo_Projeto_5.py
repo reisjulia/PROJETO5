424812a76961325470180b46ae6af3323284d0e2
Julia Reis <102388580+reisjulia@users.noreply.github.com>
2023-03-27 21:37:15 -0300

    Update Codigo_Projeto_5.py

--- Codigo_Projeto_5.py
+++ Codigo_Projeto_5.py
@@ -1,4 +1,7 @@
 import numpy as np
+import time
+import matplotlib.pyplot as plt
+
 
 matriz = [
     [1, 1, 1, 0, 0, 0],
@@ -23,6 +26,7 @@ for element in sorted(coordinates.keys()):
     for coord in coordinates[element]:
         print(f'{coord}')
 
+
 values = [0, 1, 2, 4]
 
 for i in values:
@@ -31,3 +35,25 @@ for i in values:
     quantity = np.count_nonzero(matriz2 == i)
     print(f'Elemento {elements} apareceu {quantity} vezes')
     print(f'Soma: {elements*quantity}')
+
+def fib(elements):
+    if elements <= 1:
+        return elements
+    return fib(elements - 1) + fib(elements - 2)
+
+ns = []
+tempos = []
+
+for elements in range(1, 31):
+    start = time.perf_counter()
+    result= fib(elements)
+    end = time.perf_counter()
+    ms = (end-start) * 10**6
+    ns.append(elements)
+    tempos.append(ms)
+print(result)
+# cria o gráfico
+plt.plot(ns, tempos)
+plt.xlabel('Valor de elements')
+plt.ylabel('Tempo de execução (micro segundos)')
+plt.show()
